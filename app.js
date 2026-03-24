e2ef5e24a2b1a75e988ed8c799c61c880f6b0e52
mirdayan <dmirandaherrera@gmail.com>
2026-03-23 21:51:45 -0500

    Creando e instanciando la clase de presupuesto y la clase de interfax de usuario

--- app.js
+++ app.js
@@ -7,13 +7,41 @@ const gastoListado = document.querySelector('#gasto ul');
 
 // Eventos
 
-eventListeners ();
-function eventListeners () {
+eventListeners();
+function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto)
 }
 
 
 // Classes
+
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gasto = [];
+
+    }
+
+}
+
+class UI {
+    insertarPresupuesto( cantidad) {
+        // Extrayendo los valores
+        const {presupuesto, restante } = cantidad;
+
+        // Agregando al HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+
+    }
+
+}
+
+// Instanciar la clase UI
+const ui = new UI();
+
+let presupuesto;
 
 
 
@@ -23,11 +51,21 @@ function eventListeners () {
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
 
-    console.log(Number(presupuestoUsuario));
+    //console.log(Number(presupuestoUsuario));
 
     if(presupuestoUsuario === ' ' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0 ) {
         window.location.reload();
     }
+
+    // Presupuesto valido
+
+    //instanciando la clase presupuesto
+
+    presupuesto = new Presupuesto (presupuestoUsuario);
+    console.log(presupuesto);
+
+
+    ui.insertarPresupuesto(presupuesto);
 
 
 }
